636ba7de9a487ad22f44f1d7a9a4a29660254a85
Robert Kessler <robby9903@gmail.com>
2024-11-15 16:23:36 +0100

    Added docker-compose

--- product-service/adapters/user_service_client.go
+++ product-service/adapters/user_service_client.go
@@ -22,14 +22,15 @@ type UserVerificationResponse struct {
 	IsAdmin   bool   `json:"is_admin"`
 }
 
+// VerifyToken sendet das Access-Token an den User-Service und überprüft den Benutzer
 func (c *UserServiceClient) VerifyToken(token string) (*UserVerificationResponse, error) {
 	url := fmt.Sprintf("%s/verify", c.BaseURL)
 	req, _ := http.NewRequest("GET", url, nil)
 
-	// Entferne zusätzliche Anführungszeichen
+	// Entferne zusätzliche Anführungszeichen um den Token
 	token = strings.Trim(token, "\"")
 
-	// Prüfe, ob das Präfix fehlt, und füge es hinzu
+	// Prüfe, ob das Präfix "Bearer " fehlt, und füge es bei Bedarf hinzu
 	if !strings.HasPrefix(token, "Bearer ") {
 		token = "Bearer " + token
 	}
@@ -39,16 +40,22 @@ func (c *UserServiceClient) VerifyToken(token string) (*UserVerificationResponse
 	fmt.Printf("Authorization Header sent: %s\n", req.Header)
 
 	res, err := http.DefaultClient.Do(req)
-	if err != nil || res.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(res.Body)
-		fmt.Printf("Response Body: %s\n", string(body)) // Logge die Antwort
-		return nil, fmt.Errorf("unauthorized or invalid token")
+	if err != nil {
+		fmt.Printf("Error sending request to User-Service: %v\n", err)
+		return nil, fmt.Errorf("error connecting to user service: %v", err)
 	}
 	defer res.Body.Close()
 
+	// Überprüfe den Statuscode der Antwort
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		fmt.Printf("Error response from User-Service, Status Code: %d, Body: %s\n", res.StatusCode, string(body))
+		return nil, fmt.Errorf("unauthorized or invalid token")
+	}
+
 	var userResp UserVerificationResponse
 	if err := json.NewDecoder(res.Body).Decode(&userResp); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error decoding user service response: %v", err)
 	}
 
 	return &userResp, nil
