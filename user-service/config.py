4d97438ff6233b8b55a51eb528a5dd14f589e1e7
kesslermatics <Robby9903@gmail.com>
2025-01-03 10:20:53 +0100

    Added kubernetes files to user and product services

--- user-service/config.py
+++ user-service/config.py
@@ -4,6 +4,4 @@ from datetime import timedelta
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///users.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "superduperstarkespasswort")  
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
